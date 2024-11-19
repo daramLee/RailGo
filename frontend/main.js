70f9ac8c841c7f601cb13f7b3bd927119cba3320
DaramLee <leeminju913@gmail.com>
2024-11-19 14:57:52 +0900

    refactor: 철도 노선 렌더링 관련 코드 리팩토링
    마우스 오버, 아웃 이벤트 함수가 반복되는 내용이 많아서 수정함.

--- frontend/main.js
+++ frontend/main.js
@@ -1,189 +1,118 @@
-
+import {fetchStation} from './js/api.js'; //import는 최상단에.
 window.onload = function() { //html의 DOM이 모두 로드 된 후에 js실행할 수 있게 함.
+
+    function createLineImg (mapContainer, data){
+        data.forEach(element => {
+            let lineImg = document.createElement('img'); 
+            lineImg.id = element.id; 
+            lineImg.className = 'lineImg'
+            lineImg.src = element.src;
+            mapContainer.appendChild(lineImg);
+        });
+    }
+    function deleteLineImg (mapContainer, lineIds){
+        lineIds.forEach(id =>{
+            const lineImg = document.getElementById(id);
+            mapContainer.removeChild(lineImg);
+        })
+    }
+    
     //서울 맵핀
     document.getElementById('seoul').addEventListener('mouseover', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        //innerHTML은 기존 요소 리셋해버림, 요소 생성 후 직접 접근 불가
-        //그래서 createElement&append 사용해야함.
-        const gyeongbuLineImg = document.createElement('img'); 
-        gyeongbuLineImg.id = 'gyeongbuLine';
-        gyeongbuLineImg.className = 'lineImg'
-        gyeongbuLineImg.src = 'image/gyeongbu.png';
-        
-        const gyeongjeonLineImg = document.createElement('img'); 
-        gyeongjeonLineImg.id = 'gyeongjeonLine';
-        gyeongjeonLineImg.className = 'lineImg'
-        gyeongjeonLineImg.src = 'image/gyeongjeon.png';
-
-        const gangneungLineImg = document.createElement('img'); 
-        gangneungLineImg.id = 'gangneungLine';
-        gangneungLineImg.className = 'lineImg'
-        gangneungLineImg.src = 'image/gangneung.png';
-
-        const jungangLineImg = document.createElement('img'); 
-        jungangLineImg.id = 'jungangLine';
-        jungangLineImg.className = 'lineImg'
-        jungangLineImg.src = 'image/jungang.png';
-
-        mapContainer.appendChild(gyeongbuLineImg); //appendChild는 문자열x, 노드만 삽입 가능
-        mapContainer.appendChild(gyeongjeonLineImg); 
-        mapContainer.appendChild(gangneungLineImg); 
-        mapContainer.appendChild(jungangLineImg); 
+        createLineImg(mapContainer, [
+            {id: 'gyeongbuLine', src: 'image/gyeongbu.png'},
+            {id: 'gyeongjeonLine', src: 'image/gyeongjeon.png'},
+            {id: 'gangneungLine', src: 'image/gangneung.png'},
+            {id: 'jungang', src: 'image/jungang.png'}
+            ]);
     })
     document.getElementById('seoul').addEventListener('mouseout', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        const gyeongbuLineImg = document.getElementById('gyeongbuLine');
-        const gyeongjeonLineImg = document.getElementById('gyeongjeonLine');
-        const gangneungLineImg = document.getElementById('gangneungLine');
-        const jungangLineImg = document.getElementById('jungangLine');
-        
-        mapContainer.removeChild(gyeongbuLineImg);       
-        mapContainer.removeChild(gyeongjeonLineImg);  
-        mapContainer.removeChild(gangneungLineImg); 
-        mapContainer.removeChild(jungangLineImg);    
+        deleteLineImg(mapContainer, ['gyeongbuLine', 'gyeongjeonLine', 'gangneungLine', 'jungangLine']);
     })
     //용산 맵핀
     document.getElementById('yongsan').addEventListener('mouseover', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        const honamLineImg = document.createElement('img'); 
-        honamLineImg.id = 'honamLine';
-        honamLineImg.className = 'lineImg'
-        honamLineImg.src = 'image/honam.png';
-
-        const jeonraLineImg = document.createElement('img'); 
-        jeonraLineImg.id = 'jeonraLine';
-        jeonraLineImg.className = 'lineImg'
-        jeonraLineImg.src = 'image/jeonra.png';
-        
-        
-
-        mapContainer.appendChild(honamLineImg); //appendChild는 문자열x, 노드만 삽입 가능
-        mapContainer.appendChild(jeonraLineImg); 
-     
+        createLineImg(mapContainer, [
+            {id: 'honamLine', src: 'image/honam.png'},
+            {id: 'jeonraLine', src: 'image/jeonra.png'},
+            ]);
     })
     document.getElementById('yongsan').addEventListener('mouseout', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        const honamLineImg = document.getElementById('honamLine');
-        const jeonraLineImg = document.getElementById('jeonraLine');
-        
-        mapContainer.removeChild(honamLineImg);       
-        mapContainer.removeChild(jeonraLineImg);  
- 
+        deleteLineImg(mapContainer, ['honamLine', 'jeonraLine']);
     })
     
     //수원 맵핀
-    document.getElementById('suwon').addEventListener('mouseenter', (event) =>{
+    document.getElementById('suwon').addEventListener('mouseover', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        //innerHTML은 기존 요소 리셋해버림, 요소 생성 후 직접 접근 불가
-        //그래서 createElement&append 사용해야함.
-        const gyeongbuLineImg = document.createElement('img'); 
-        gyeongbuLineImg.id = 'gyeongbuLine';
-        gyeongbuLineImg.className = 'lineImg'
-        gyeongbuLineImg.src = 'image/gyeongbu.png';
-
-        mapContainer.appendChild(gyeongbuLineImg); //appendChild는 문자열x, 노드만 삽입 가능
-        console.log("mouse enter ing");
+        createLineImg(mapContainer, [
+            {id: 'gyeongbuLine', src: 'image/gyeongbu.png'},
+            ]);
     })
-    document.getElementById('suwon').addEventListener('mouseleave', (event) =>{
+    document.getElementById('suwon').addEventListener('mouseout', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        const gyeongbuLineImg = document.getElementById('gyeongbuLine');
-
-        mapContainer.removeChild(gyeongbuLineImg);       
-
+        deleteLineImg(mapContainer, ['gyeongbuLine']);
     })
     //오송 맵핀
     document.getElementById('osong').addEventListener('mouseover', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        //innerHTML은 기존 요소 리셋해버림, 요소 생성 후 직접 접근 불가
-        //그래서 createElement&append 사용해야함.
-        const gyeongbuLineImg = document.createElement('img'); 
-        gyeongbuLineImg.id = 'gyeongbuLine';
-        gyeongbuLineImg.className = 'lineImg'
-        gyeongbuLineImg.src = 'image/gyeongbu.png';
-        
-        const gyeongjeonLineImg = document.createElement('img'); 
-        gyeongjeonLineImg.id = 'gyeongjeonLine';
-        gyeongjeonLineImg.className = 'lineImg'
-        gyeongjeonLineImg.src = 'image/gyeongjeon.png';
-
-        const honamLineImg = document.createElement('img'); 
-        honamLineImg.id = 'honamLine';
-        honamLineImg.className = 'lineImg'
-        honamLineImg.src = 'image/honam.png';
-
-        const jeonraLineImg = document.createElement('img'); 
-        jeonraLineImg.id = 'jeonraLine';
-        jeonraLineImg.className = 'lineImg'
-        jeonraLineImg.src = 'image/jeonra.png';
-        
-        mapContainer.appendChild(gyeongbuLineImg); //appendChild는 문자열x, 노드만 삽입 가능
-        mapContainer.appendChild(gyeongjeonLineImg); 
-        mapContainer.appendChild(honamLineImg); //appendChild는 문자열x, 노드만 삽입 가능
-        mapContainer.appendChild(jeonraLineImg); 
+        createLineImg(mapContainer, [
+            {id: 'gyeongbuLine', src: 'image/gyeongbu.png'},
+            {id: 'gyeongjeonLine', src: 'image/gyeongjeon.png'},
+            {id: 'honamLine', src: 'image/honam.png'},
+            {id: 'jeonraLine', src: 'image/jeonra.png'}
+            ]);
     })
     document.getElementById('osong').addEventListener('mouseout', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        const gyeongbuLineImg = document.getElementById('gyeongbuLine');
-        const gyeongjeonLineImg = document.getElementById('gyeongjeonLine');
-        const honamLineImg = document.getElementById('honamLine');
-        const jeonraLineImg = document.getElementById('jeonraLine');
-        
-        mapContainer.removeChild(gyeongbuLineImg);       
-        mapContainer.removeChild(gyeongjeonLineImg);  
-        mapContainer.removeChild(honamLineImg);       
-        mapContainer.removeChild(jeonraLineImg);  
- 
+        deleteLineImg(mapContainer, 
+            ['gyeongbuLine', 'gyeongjeonLine', 'honamLine', 'jeonraLine']
+        );
     })
     //광주송정 맵핀
     document.getElementById('gwangjusongjeong').addEventListener('mouseover', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        //innerHTML은 기존 요소 리셋해버림, 요소 생성 후 직접 접근 불가
-        //그래서 createElement&append 사용해야함.
-        const honamLineImg = document.createElement('img'); 
-        honamLineImg.id = 'honamLine';
-        honamLineImg.className = 'lineImg'
-        honamLineImg.src = 'image/honam.png';
-
-        mapContainer.appendChild(honamLineImg); //appendChild는 문자열x, 노드만 삽입 가능
- 
+        createLineImg(mapContainer, [
+            {id: 'honamLine', src: 'image/honam.png'}
+            ]);
     })
     document.getElementById('gwangjusongjeong').addEventListener('mouseout', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        const honamLineImg = document.getElementById('honamLine');
-        
-        mapContainer.removeChild(honamLineImg);       
-  
+        deleteLineImg(mapContainer, ['honamLine']);
     })
     //부산 맵핀
     document.getElementById('pusan').addEventListener('mouseover', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        //innerHTML은 기존 요소 리셋해버림, 요소 생성 후 직접 접근 불가
-        //그래서 createElement&append 사용해야함.
-        const gyeongbuLineImg = document.createElement('img'); 
-        gyeongbuLineImg.id = 'gyeongbuLine';
-        gyeongbuLineImg.className = 'lineImg'
-        gyeongbuLineImg.src = 'image/gyeongbu.png';
-
-        mapContainer.appendChild(gyeongbuLineImg); //appendChild는 문자열x, 노드만 삽입 가능
-
+        createLineImg(mapContainer, [
+            {id: 'gyeongbuLine', src: 'image/gyeongbu.png'}
+            ]);
     })
     document.getElementById('pusan').addEventListener('mouseout', (event) =>{
         const mapContainer = document.getElementById('mapContainer');
 
-        const gyeongbuLineImg = document.getElementById('gyeongbuLine');
-        
-        mapContainer.removeChild(gyeongbuLineImg);       
-        
+        deleteLineImg(mapContainer, ['gyeongbuLine']);
     })
+ 
+    const input = document.getElementById('searchInput');
+    const data = {};
+    input.addEventListener('keyup', (event) =>{ //html에서 onkeyup말고 js에서 이벤트처리해야 유지보수하기 좋음.
+        if(event.key == 'Enter'){
+            fetchStation(input.value).then((response) =>{
+                console.log(response);
+            })
+        }
+    }) 
 }
